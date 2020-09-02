7dacbd0cff2db25d26247763fd75239d9c1fc78b
rahalrazika <rahalraika@gmail.com>
2020-09-02 10:48:55 +0200

    add validation to the post modle and set up assosiation between post ans user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :body, presence: true
+    validates :user_id, presence: true
+
+    belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     validates :username, presence: true
     validates :password, presence: true
     validates :email, presence: true
+
+    has_many :posts
 end
